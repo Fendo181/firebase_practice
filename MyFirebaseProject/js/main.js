92967348b4df631b8b0d8aa3716926f019de16a9
Fendo181 <fendo181git@gmail.com>
2019-01-12 21:40:15 +0900

    コードを整形した

--- MyFirebaseProject/js/main.js
+++ MyFirebaseProject/js/main.js
@@ -1,13 +1,13 @@
 'use strict';
 const config = {
-    apiKey: "AIzaSyBE3EVMus-fdY98en7HdmV1UZmRQ9Sat14",
-    authDomain: "chatapp-dt1201.firebaseapp.com",
-    databaseURL: "https://chatapp-dt1201.firebaseio.com",
-    projectId: "chatapp-dt1201",
-    storageBucket: "chatapp-dt1201.appspot.com",
-    messagingSenderId: "720323939081"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyBE3EVMus-fdY98en7HdmV1UZmRQ9Sat14",
+  authDomain: "chatapp-dt1201.firebaseapp.com",
+  databaseURL: "https://chatapp-dt1201.firebaseio.com",
+  projectId: "chatapp-dt1201",
+  storageBucket: "chatapp-dt1201.appspot.com",
+  messagingSenderId: "720323939081"
+};
+firebase.initializeApp(config);
 const db = firebase.firestore();
 
 db.settings({
@@ -22,9 +22,9 @@ const form = document.querySelector('form');
 const messages = document.getElementById('messages');
 
 // 入力した値を表示する
-collection.orderBy('created').onSnapshot( snapshot => {
-  snapshot.docChanges().forEach( change => {
-    if(change.type === 'added'){
+collection.orderBy('created').onSnapshot(snapshot => {
+  snapshot.docChanges().forEach(change => {
+    if (change.type === 'added') {
       const li = document.createElement('li');
       li.textContent = change.doc.data().message;
       // messages に対して、子要素として li を追加
@@ -39,7 +39,7 @@ form.addEventListener('submit', e => {
 
   // 入力したデータを検査する
   const val = message.value.trim();
-  if (val === ""){
+  if (val === "") {
     // 空文字の場合は処理をしない。
     alert('空文字を入力しないで下さい');
     return;
@@ -51,15 +51,15 @@ form.addEventListener('submit', e => {
 
   // 保存する
   collection.add({
-    message:val,
-    created: firebase.firestore.FieldValue.serverTimestamp()
-  })
-  .then(doc => {
-    console.log(`${doc.id}.added!`)
-  })
-  .catch(error => {
-    console.log(error);
-  });
+      message: val,
+      created: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    .then(doc => {
+      console.log(`${doc.id}.added!`)
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 message.focus();

711ea67f6f05f7e1f2359c89c77912c15fac24a5
Fendo181 <fendo181git@gmail.com>
2019-01-12 20:25:34 +0900

    chap3 firebaseを利用するスクリプトを作成する

---
+++ MyFirebaseProject/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+const config = {
+    apiKey: "AIzaSyBE3EVMus-fdY98en7HdmV1UZmRQ9Sat14",
+    authDomain: "chatapp-dt1201.firebaseapp.com",
+    databaseURL: "https://chatapp-dt1201.firebaseio.com",
+    projectId: "chatapp-dt1201",
+    storageBucket: "chatapp-dt1201.appspot.com",
+    messagingSenderId: "720323939081"
+  };
+  firebase.initializeApp(config);

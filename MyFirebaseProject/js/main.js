2e6a59e6017a84dac197a372e9ca6833088789d7
Fendo181 <fendo181git@gmail.com>
2019-01-12 20:48:12 +0900

    firestoreにデータを保存しり処理を追加した

--- MyFirebaseProject/js/main.js
+++ MyFirebaseProject/js/main.js
@@ -1,6 +1,4 @@
 'use strict';
-
-
 const config = {
     apiKey: "AIzaSyBE3EVMus-fdY98en7HdmV1UZmRQ9Sat14",
     authDomain: "chatapp-dt1201.firebaseapp.com",
@@ -10,3 +8,21 @@ const config = {
     messagingSenderId: "720323939081"
   };
   firebase.initializeApp(config);
+
+const db = firebase.firestore();
+
+db.settings({
+  timestampsInSnapshots: true
+});
+// messages という Collection がなかった場合は新しく作ってくれる
+const collection = db.collection('mxessages');
+
+collection.add({
+  message:'test'
+})
+.then(doc => {
+  console.log(`${doc.id}.added!`)
+})
+.catch(error => {
+  console.log(error);
+});
